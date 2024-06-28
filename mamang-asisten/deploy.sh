bcc2602e596551bfc4c44475d0fd413075eccdd5
Imre Nagi <imre.nagi2812@gmail.com>
2024-06-28 16:06:19 +0200

    fix: add tools input class and refine the prompt
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>

--- mamang-asisten/deploy.sh
+++ mamang-asisten/deploy.sh
@@ -1,1 +1,3 @@
+gcloud run deploy mamang-asisten --env-vars-file=vars.yml  --allow-unauthenticated --region us-central1 --image us-central1-docker.pkg.dev/imrenagi-gemini-experiment/cloud-run-source-deploy/mamang-asisten:latest
+
 gcloud run deploy mamang-asisten --env-vars-file=vars.yml  --allow-unauthenticated --source . --region us-central1
